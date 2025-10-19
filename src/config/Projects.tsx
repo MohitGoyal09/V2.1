0d7b5002847fdb32cc866bcb4587f8b9d7081c2c
Mohit <goel0632@gmail.com>
2025-10-20 03:00:56 +0530

    Refactor Projects configuration by removing unused technology imports

--- src/config/Projects.tsx
+++ src/config/Projects.tsx
@@ -1,15 +1,9 @@
-
 import NextJs from '@/components/technologies/NextJs';
-import NodeJs from '@/components/technologies/NodeJs';
-import PostgreSQL from '@/components/technologies/PostgreSQL';
 import Prisma from '@/components/technologies/Prisma';
 import Pytorch from '@/components/technologies/Pytorch';
 import ReactIcon from '@/components/technologies/ReactIcon';
-import Sanity from '@/components/technologies/Sanity';
 import Shadcn from '@/components/technologies/Shadcn';
-import SocketIo from '@/components/technologies/SocketIo';
 import TailwindCss from '@/components/technologies/TailwindCss';
-import ThreeJs from '@/components/technologies/ThreeJs';
 import TypeScript from '@/components/technologies/TypeScript';
 import Vercel from '@/components/technologies/Vercel';
 import { Project } from '@/types/project';
