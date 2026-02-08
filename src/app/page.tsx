fb91b8c6eaa567b72d8be4cf2884a0151d5462df
Mohit <goel0632@gmail.com>
2026-02-08 22:43:41 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import { getPublishedBlogPosts } from '@/lib/blog';
 
 import LandingPageClient from './LandingPageClient';
 
+import { Suspense } from 'react';
+
 export default function LandingPage() {
   const blogPosts = getPublishedBlogPosts();
 
-  return <LandingPageClient blogPosts={blogPosts} />;
+  return (
+    <Suspense>
+      <LandingPageClient blogPosts={blogPosts} />
+    </Suspense>
+  );
 }
